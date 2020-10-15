9cfbec67db1d76ca5cdde68076bcdb9dac5b6d0b
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2020-10-15 09:17:44 +0300

    Add files via upload

---
+++ problema11.py
@@ -0,0 +1,7 @@
+a=int(input("dati primul numar"))
+b=int(input("dati al doilea numar"))
+c=int(input("dati al treilea numar"))
+if (a%2=0) and (b%2=0) and (c%2=0):
+    print(a,"par", b ,"par", c ,"par")
+if (a%2!=0) or (b%2!=0) or (c%2!=0):
+    print(a, "impar", b ,"par", c ,"par")

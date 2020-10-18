e26afe4167557c7338723792cbca5a79a23682d8
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2020-10-18 11:39:07 +0300

    Add files via upload

---
+++ exercitiul4.py
@@ -0,0 +1,9 @@
+a=int(input("dati virsta primei persoane: "))
+b=int(input("dati virsta persoanei a doua: "))
+c=int(input("dati virsta persoanei a treia: "))
+if (a>18) and (a<60):
+   print(a)
+if (b>18) and (b<60):
+   print(b)
+if (c>18) and (c<60):
+   print(c)

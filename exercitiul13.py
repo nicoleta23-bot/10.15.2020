7b60b6838056f7f896d0fd889ca1f2ccef61827e
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2020-10-18 20:24:39 +0300

    Add files via upload

---
+++ exercitiul13.py
@@ -0,0 +1,9 @@
+x=int(input("introduceti locul ocupat de Ionel: "))
+if (100//x==3):
+    print("alba ")
+if (100//x==2):
+    print("rosie ")
+if (100//x==1):
+    print("albastra ")
+if (100//x==0):
+    print("neagra ") 

fe5c21f8580e0c050a182545a4e6f6d4a5e87e8f
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2020-10-18 12:39:29 +0300

    Add files via upload

---
+++ exercitiul8.py
@@ -0,0 +1,12 @@
+a=int(input("dati primul numar: "))
+b=int(input("dati al doilea numar: "))
+if (a%2==0) and (b%2==0):
+    if (a>b):
+        print(a)
+    else: print(b)
+if (a%2==0) and (b%2!=0):
+    print(a)
+if (a%2!=0) and (b%2==0):
+    print(b)
+if (a%2!=0) and (b%2!=0):
+    print("nu exista numar par")

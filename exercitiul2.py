b6a6d668dfc705a11b1e13ad074eb7a9a92b8853
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2020-10-18 10:17:12 +0300

    Add files via upload

---
+++ exercitiul2.py
@@ -0,0 +1,6 @@
+a=int(input("dati primul nr: "))
+b=int(input("dati al doilea nr: "))
+c=int(input("dati al treilea nr: "))
+if (a<(b+c)) and (b<(a+c)) and (c<(a+b)):
+    print("da")
+else: print("nu")

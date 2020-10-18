a344d4fa525e2557386ee68e7347b9f8054f60d6
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2020-10-18 20:38:11 +0300

    Add files via upload

---
+++ exercitiul15.py
@@ -0,0 +1,10 @@
+x=int(input("dati locul lui Radu: "))
+if x//25==0: 
+    print("A") 
+elif x//25==1: 
+    print("B") 
+elif x//25==2: 
+    print("C") 
+elif x//25==3: 
+    print("D") 
+else: print("E")

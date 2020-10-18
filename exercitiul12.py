c333d09d96e738d8c3e93a3345bd6cc1555f6bf8
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2020-10-18 20:20:04 +0300

    Add files via upload

---
+++ exercitiul12.py
@@ -0,0 +1,10 @@
+x=int(input("Numarul de petale"))   
+if x%5==1: 
+    print("ma iubeste un pic") 
+elif x%5==2: 
+    print("ma iubeste mult")
+elif x%5==3: 
+    print("ma iubeste cu pasiune") 
+elif x%5==4: 
+    print("ma iubeste la nebunie") 
+else: print("ma iubeste de loc")
